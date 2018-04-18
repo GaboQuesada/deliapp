295992166883d90e05c72293f5778d57c459f091
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-04-18 14:54:14 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

---
+++ navbar.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="stylesheet" href="lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" >
+        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="2.css">
+        <link rel="stylesheet" href="3.css">
+        <script type="text/javascript" src="lib/jquery/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+
+
+    <body>
+
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span> 
+                    </button>
+                    <img src="img/logo.png" width="45" height="45" class="" />
+                    
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    
+                   
+                    
+                    <ul class="nav navbar-nav">
+                        <li class="active">&numsp;</li>
+                        <li class="active"><button class="btn btn-default navbar-btn">Ordenar</button></li>
+                        <li class="active">&numsp;</li>
+                         <li class="active"> <button class="btn btn-primary navbar-btn">Facturar</button></li>
+                         <li class="active">&numsp;</li>
+                        <li ><a href="#">SPV</a></li>
+                        <li><a href="#">INV</a></li>
+                        <li><a href="#">ADM</a></li> 
+                       
+                    </ul>
+
+                    
+
+
+
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li> <form class="navbar-form navbar-left" action="/action_page.php">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-default" type="submit">
+                                            <i class="glyphicon glyphicon-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form></li>
+
+                        <li class="dropdown"><a class="dropdown-toggle glyphicon glyphicon-user" data-toggle="dropdown" href="#">Sign Up<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Page 1-1</a></li>
+                                <li><a href="#">Page 1-2</a></li>
+                                <li><a href="#">Page 1-3</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#"><span class="badge">10</span></a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+<!--
+        <div class="container-fluid containerbox">
+           
+                <div class="col-md-2  bloqueizquierdo">
+                    <div class="sidebar-nav">
+                        <div class="navbar navbar-default" role="navigation">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <span class="visible-xs navbar-brand">Sidebar menu</span>
+                            </div>
+                            <div class="navbar-collapse collapse sidebar-navbar-collapse">
+                                <ul class="nav navbar-nav">
+                                    <li><a href="#">Menu Item 1</a></li>
+                                    <li><a href="#">Menu Item 2</a></li>
+                                    <li><a href="#">Menu Item 3</a></li>
+                                    <li><a href="#">Menu Item 4</a></li>
+                                    <li><a href="#">Reviews <span class="badge">1,118</span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-10 bloquederecho"> 
+                  
+                   
+                </div>
+           
+        </div>
+
+-->
+    </body>
+</html>
