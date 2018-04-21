659ec9e7858f670fd234911c702ea607e7af6dea
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-04-21 13:09:41 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

--- appCore/controler/user/usuarioPassCheck.js
+++ appCore/controler/user/usuarioPassCheck.js
@@ -1,2 +1,36 @@
 
 
+function logIn(){
+    
+    
+    var pus = $("#txtUs").val();
+    var pps = $("#txtPs").val();
+    
+    
+    
+     $.ajax({
+        url: "appCore/model/user/usuarioPassCheckCount.php",
+        type: 'POST',
+        dataType: "json",
+        data: {us: pus, ps: pps},
+        beforeSend: function () {
+
+        },
+        success: function (respuesta) {
+
+            var tam;
+            var datosrespuesta = respuesta.resultados;
+            $("#tablaresultados").empty();
+            $.each(datosrespuesta, function (i, item) {
+                tam = tam + 1;
+                $("#tablaresultados").append("<tr data-toggle='modal' data-target='#instructorModalConfirmInsert'   id='" + datosrespuesta[i].PER_CODIGO + "' onclick='test(" + datosrespuesta[i].PER_CODIGO + ")'><td>" + datosrespuesta[i].PER_CODIGO + "</td><td>" + datosrespuesta[i].PER_NOMBRE + "</td><td>" + datosrespuesta[i].PER_P_APELLIDO + "</td><td>" + datosrespuesta[i].PER_S_APELLIDO + "</td></tr>");
+            });
+        },
+        error: function () {
+
+        }
+    }); 
+    
+    
+}
+
