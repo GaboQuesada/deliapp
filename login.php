82589590e8d766eed6df8b788d019da88d96005c
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-05-17 10:18:59 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

--- login.php
+++ login.php
@@ -8,31 +8,48 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+         <link rel="stylesheet" href="libx/animation/css/animation.css" >
         <link rel="stylesheet" href="css/login.css" >
 
         <title>Hello, world!</title>
     </head>
     <body class="text-center">
-        <form class="form-signin">
-            <img class="mb-4" src="img/logo.png" alt="" width="150" height="150">
-            
-            <div class="input-group mb-2">
-                <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fas fa-user-alt"></i></div>
-                </div>
-                <input type="email" id="inputEmail" class="form-control form-control-sm" placeholder="Usuario" required autofocus>
+
+
+        
+
+<div class="centrado-porcentual">
+            <div class="caja1 slideRight " id="caja1">
+<img class="mb-4" src="img/logo.png" alt="" width="200" height="200">
             </div>
-            <div class="input-group mb-2">
+            <div class="caja2 slideDown" id="caja2">
+  <form class="form-signin">
+            
+            
+            
+            <div class="input-group mb-4">
                 <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fas fa-key"></i></div>
+                    <div class="input-group-text"><i class="fas fa-user-alt tamf"></i></div>
                 </div>
-                <input type="password" id="inputPassword" class="form-control form-control-sm" placeholder="Contraseña" required>
+                <input type="email" id="inputEmail" class="form-control colf " placeholder="Usuario" required autofocus>
+            </div>
+            <div class="input-group mb-4">
+                <div class="input-group-prepend">
+                    <div class="input-group-text"><i class="fas fa-key tamf"></i></div>
+                </div>
+                <input type="password" id="inputPassword" class="form-control colf" placeholder="Contraseña" required>
             </div>
 
 
             <button class="btn btn btn-dark btn-block" type="submit"><i class="fas fa-unlock"></i> Entrar</button>
 
         </form>
+            </div>
+
+        </div>
+
+
+
 
 
         <!-- Optional JavaScript -->
@@ -40,7 +57,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-        <script src="lib/animation/js/animation.js" ></script>
+        <script src="libx/animation/js/animation.js" ></script>
 
     </body>
 </html>
