4cdbc7d52f4ce26ddb225afa9688781c1a24deb2
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-05-23 21:15:18 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

---
+++ syModel/planilla/getCount.php
@@ -0,0 +1,29 @@
+<?php
+include '../../bd/connect.php';
+$conexion = new Connect();
+$conn = $conexion->conect();
+
+
+        try {
+            $stmt = $conn->prepare("CALL PLANILLAgetCount()");
+            
+
+        
+                $stmt->execute();
+                $can=$stmt->fetchColumn();
+                $respuesta['estado'] = "1";
+                $respuesta['mensajelog'] = "Consulta Exitosa (getAll)";
+                $respuesta['mensaje'] = "Consulta Exitosa.";
+                $respuesta['resultados'] = $can;
+                print json_encode($respuesta);
+            
+            
+        } catch (PDOException $e) {
+
+            $respuesta['estado'] = "0";
+            $respuesta['mensajelog'] = $e->getMessage();
+            $respuesta['mensaje'] = "Ha ocurrido un error.";
+             print json_encode($respuesta);
+        }
+    
+    ?>
