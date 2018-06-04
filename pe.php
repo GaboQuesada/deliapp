9d88ec2657c9e2288c634042314c2badd63a6900
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-06-04 12:32:26 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

--- pe.php
+++ pe.php
@@ -275,27 +275,17 @@
             <!-- Optional JavaScript -->
             <!-- jQuery first, then Popper.js, then Bootstrap JS -->
             <script src="libx/jquery/jquery-3.2.1.min.js" ></script>
-            <script src="syController/planilla/personagetAll.js" ></script>
-            <script src="syController/planilla/personaInsert.js" ></script>
+       
+     
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
             <script src="libx/animation/js/animation.js" ></script>
             <script src="libx/alertifyjs/js/alertify.js" ></script>
             <script src="js/index.js" ></script>
             <script src="js/validaciones.js" ></script>
+            <script src="personagetAll.js" ></script>
          
-            <script type="text/javascript" >
-
-                $(document).ready(function () {
-
-                    show(0);
-
-
-
-                });
-
-
-            </script>
+          
 
 
 
