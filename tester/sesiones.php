022989bf3ee2cb97489f49b04cdfe59d0455cab5
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-07-17 12:36:52 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

---
+++ tester/sesiones.php
@@ -0,0 +1,11 @@
+<?php
+
+echo $_SESSION["usuarioid"] = $usuarioid;
+echo $_SESSION["usuario"] = $usuario;
+echo $_SESSION["usuarioIMG"] = $usuarioIMG;
+echo $_SESSION["nombre"] = $nombre;
+echo $_SESSION["rolid"] = $rolid;
+echo $_SESSION["ronombre"] = $ronombre;
+echo $_SESSION["departamento"] = $departamento;
+echo $_SESSION["cargo"] = $cargo;
+?>
