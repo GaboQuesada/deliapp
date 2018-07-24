39797c6c7ae8106afb0824831e1d04227274e2b2
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-07-24 10:55:58 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

--- compPlanilla/departamentos.php
+++ compPlanilla/departamentos.php
@@ -19,6 +19,8 @@ if (!empty($_SESSION["usuarioid"]) && $_SESSION["Departamentos"] == 1) {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
         <link rel="stylesheet" href="../lib/animation/css/animation.css" >
+        <link rel="stylesheet" href="../lib/alertifyjs/css/alertify.css" >
+        <link rel="stylesheet" href="../lib/alertifyjs/css/themes/semantic.min.css" >
         <link rel="stylesheet" href="css/menusuperior.css" >
         <link rel="stylesheet" href="css/boxmodel.css" >
         <link rel="stylesheet" href="css/boxcuerpo.css" >
--- compPlanilla/personal.php
+++ compPlanilla/personal.php
@@ -1,12 +1,10 @@
 <?php
 @session_start();
 
-
-
-if (!empty($_SESSION["usuarioid"])) {
+if (!empty($_SESSION["usuarioid"]) && $_SESSION["Agregar a Planilla"] == 1) {
     
 } else {
-    header("Location: ../index.php");
+    header("Location: ../comps/nomodule.php");
 }
 ?>
 
@@ -21,8 +19,11 @@ if (!empty($_SESSION["usuarioid"])) {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
         <link rel="stylesheet" href="../lib/animation/css/animation.css" >
+        <link rel="stylesheet" href="../lib/alertifyjs/css/alertify.css" >
+        <link rel="stylesheet" href="../lib/alertifyjs/css/themes/semantic.css" >
         <link rel="stylesheet" href="css/menusuperior.css" >
         <link rel="stylesheet" href="css/boxmodel.css" >
+        <link rel="stylesheet" href="css/boxcuerpo.css" >
         <link rel="stylesheet" href="css/menuseccion.css" >
         <link rel="stylesheet" href="css/planilla.css" >
 
@@ -38,47 +39,20 @@ if (!empty($_SESSION["usuarioid"])) {
             <div class="caja1 " id="caja1">
                 <?php include './navlateral.php'; ?>
             </div>
-            <div class="caja2 container" id="caja2">
-                <div class="col-lg-12">
-                    <!-- Cuerpo -->
+           <div class="caja2" id="caja2">
+                <div id="cuerpobox" class="container">
+                    <div class="col-lg-12 ">
+                        <!-- Cuerpo -->
 
+                        <div class="col-12 cajaEncabezado" >
 
+                            <div class="col-3 align-self-center columEncabezado" ><img src="../img/iconos/departamento.png">Planilla</div>
+                            <div class="col-5 align-self-center columEncabezado" > <input type="text" id="actsearch" class="form-control form-control-sm" placeholder="Buscar" aria-label="Username" aria-describedby="basic-addon1"></div>
+                            <div class="col-3 align-self-center columEncabezado" ><button  class="btn btn-primary  btn-sm" data-toggle="modal" data-target="#newCat" ><i class="fas fa-plus-square"></i> Agregar</button> </div>
 
-
-
-
-                    <div class="col-lg-12" >
-                        <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item active" aria-current="page"><img src="../img/iconos/planilla.png">Planilla &nbsp; <button  class="btn btn-primary  btn-sm" data-toggle="modal" data-target="#newCat" ><i class="fas fa-plus-square"></i> Agregar</button>  
-                                </li> 
-                            </ol>
-                        </nav>
-
-
-
-
-                    </div>
-
-
-
-
-                    <div class="col-lg-12">
-
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
-                            </div>
-                            <input type="text" id="actsearch" class="form-control" placeholder="Buscar" aria-label="Username" aria-describedby="basic-addon1">
                         </div>
 
-                    </div>
-
-                    <div class="col-lg-12" >
-
-
-                        <div class="col-lg-12" id="actividadesBox">
+                        <div  id="actividadesBox" class="mainshowbox mainshowboxStyle">
 
 
                         </div>
@@ -87,6 +61,8 @@ if (!empty($_SESSION["usuarioid"])) {
 
 
                     </div>
+                    <br>
+                    <hr>
                     <div class="col-lg-12 rounded" style=" padding: 2px; color:black;" >
 
                         <div class=" col-lg-11 nav justify-content-center rounded">
@@ -338,6 +314,8 @@ if (!empty($_SESSION["usuarioid"])) {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
         <script src="../lib/animation/js/animation.js" ></script>
+        <script src="../lib/alertifyjs/js/alertify.js" ></script>
+        <script src="js/validaciones.js" ></script>
 
         <script src="controler/personasgetAllFull.js" ></script>
         <script src="controler/putInModalUpDatePlanilla.js" ></script>
