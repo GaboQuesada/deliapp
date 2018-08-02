f876d2775c895b2c6e77657abe485ed12b30e4ab
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-08-02 14:20:11 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

--- compSistema/modulos.php
+++ compSistema/modulos.php
@@ -30,14 +30,14 @@ if (!empty($_SESSION["usuarioid"]) && $_SESSION["Modulos"] == 1) {
         <link rel="stylesheet" href="css/menuseccion.css" >
         <link rel="stylesheet" href="css/modulos.css" >
         <link rel="shortcut icon" href="../img/favicon.ico">
-         <link rel="stylesheet" href="css/showinfo.css" >
+        <link rel="stylesheet" href="css/showinfo.css" >
 
         <title>Modulos</title>
     </head>
     <body>
         <a id="iraAncla" href="#ancla"></a>
         <input type="hidden" id="tocompare">
-         <?php include_once './ShowInfo.php'; ?>
+        <?php include_once './ShowInfo.php'; ?>
         <?php include './navsuperior.php'; ?>
 
 
@@ -140,7 +140,7 @@ if (!empty($_SESSION["usuarioid"]) && $_SESSION["Modulos"] == 1) {
                         <p id="modes"></p>
                         <label><strong>Sub Modulos:</strong></label>
                         <ul id="listsubmodulos" class="list-group" style="text-decoration:none; list-style:none;">
-                          
+                         
                         </ul>
                     </div>
                     <div class="modal-footer">
@@ -159,7 +159,7 @@ if (!empty($_SESSION["usuarioid"]) && $_SESSION["Modulos"] == 1) {
         <script src="../lib/alertifyjs/js/alertify.js" ></script>
 
         <script src="controler/MODULOSGetAll.js" ></script>
-          <script src="js/showinfo.js" ></script>
+        <script src="js/showinfo.js" ></script>
 
 
 
