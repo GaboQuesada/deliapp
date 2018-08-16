22680dac22cca1c20ac43ec762bd5a0bae1a8309
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-08-16 13:32:10 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

--- compSistema/controler/LocalidadesgetById.js
+++ compSistema/controler/LocalidadesgetById.js
@@ -1,22 +1,48 @@
 
 
-function getbyid(pid) {
+function morinfo(pid) {
+
     $.ajax({
         url: "model/LocalidadesGetById.php",
         type: 'POST',
         dataType: "json",
-        data: {pid:pid},
+        data: {pid: pid},
         beforeSend: function () {
 
         },
         success: function (respuesta) {
 
 
-            var d= respuesta.resultados;
-           
+            var d = respuesta.resultados;
+
             $.each(d, function (i, item) {
 
-               //d[i].comp
+
+                $("#nb").text(d[i].loc_nb);
+                $("#ce").text(d[i].loc_ce);
+                $("#tl").text(d[i].til_nb);
+                $("#de").text(d[i].loc_de);
+                $("#di").text(d[i].loc_dir);
+                $("#web").text(d[i].loc_web);
+                $("#co").text(d[i].loc_co);
+                $("#fb").text(d[i].loc_fa);
+                $("#tel").text(d[i].loc_tel);
+
+                
+
+                if (d[i].loc_img === null || d[i].loc_img === "") {
+
+                    $("#imgp").empty();
+                    $("#imgp").append("<img  src='../img/imagen.png' width='85' height='85'>");
+
+
+
+                } else {
+                    $("#imgp").empty();
+                    $("#imgp").append("<img  src='logoslocal/" + d[i].loc_img + "' width='105' height='95'>");
+
+
+                }
 
             });
         },
