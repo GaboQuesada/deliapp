8146e642d0c35ec9cf1ef53182d7f8726a0c6243
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-08-20 15:25:27 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

--- comps/fecha.php
+++ comps/fecha.php
@@ -1,29 +1,19 @@
-<?php 
-echo date("d-m-Y h:i:s A"); 
-?>
-<hr>
 <?php
+echo time();
+echo "<br>";
 
-
-if (date_default_timezone_get()) {
-    echo 'date_default_timezone_set: ' . date_default_timezone_get() . '<br />';
-}
-
-if (ini_get('date.timezone')) {
-    echo 'date.timezone: ' . ini_get('date.timezone');
-}
-
-?>
-<hr> 
-
-<?php
+echo date("d-m-Y h:i:s A O e");
+echo "<br>";
+echo "<br>";
 date_default_timezone_set('America/Costa_Rica');
+echo date("d-m-Y h:i:s A O e");
+echo "<br>";
 
-$script_tz = date_default_timezone_get();
 
-if (strcmp($script_tz, ini_get('date.timezone'))){
-    echo 'La zona horaria del script difiere de la zona horaria de la configuracion ini.';
-} else {
-    echo 'La zona horaria del script y la zona horaria de la configuración ini coinciden.';
-}
+
+
+
+
+
+
 ?>
---
+++ comps/fecha_2.php
@@ -0,0 +1,61 @@
+<?php
+echo time();
+echo "<br>";
+echo date("d-m-Y h:i:s A");
+echo "<br>";
+echo $_SERVER['DOCUMENT_ROOT'];
+echo "<br>";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_PROTOCOL'];
+echo "<br>";
+echo $_SERVER['REQUEST_METHOD'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT_CHARSET'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT_ENCODING'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+echo "<br>";
+echo $_SERVER['HTTP_CONNECTION'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['REMOTE_ADDR'];
+echo "<br>";
+echo $_SERVER['REMOTE_HOST'];
+echo "<br>";
+echo $_SERVER['REMOTE_PORT'];
+echo "<br>";
+echo $_SERVER['REMOTE_USER'];
+echo "<br>";
+echo $_SERVER['REDIRECT_REMOTE_USER'];
+echo "<br>";
+echo $_SERVER['SERVER_PORT'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT_ENCODING'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT_ENCODING'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT_ENCODING'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT_ENCODING'];
+echo "<br>";
+echo $_SERVER['HTTP_ACCEPT_ENCODING'];
+
+
+
+
+
+
+
+
+?>
